fafbac7fb3363a7d64beab41b23e794df15f4744
bthanh18391 <30694898+bthanh18391@users.noreply.github.com>
2021-05-10 22:20:16 +0700

    Update p2.sh

--- thanhmlzz/p2.sh
+++ thanhmlzz/p2.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-bash <(curl -s https://raw.githubusercontent.com/fablebox/susuwatari/master/thanhmlzz/p3.sh)
+bash <(curl -s https://raw.githubusercontent.com/bthanh18391/susuwatari/master/thanhmlzz/p3.sh)
